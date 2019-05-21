06b073f741c77a46ecce768b76624424d3229679
Nat! <nat@mulle-kybernetik.com>
2019-05-22 00:11:58 +0200

    modernized mulle-sde

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
-#define MULLE_FOUNDATION_VERSION   ((0 << 20) | (14 << 8) | 1)
+#define MULLE_FOUNDATION_VERSION   ((0 << 20) | (15 << 8) | 0)
 
 // must be kept like this, this is how the compiler reads it
 // only code that includes the foundation header will get the
 // benefit of version checking at runtime!
 
 #define MULLE_FOUNDATION_VERSION_MAJOR  0
-#define MULLE_FOUNDATION_VERSION_MINOR  14
+#define MULLE_FOUNDATION_VERSION_MINOR  15
 #define MULLE_FOUNDATION_VERSION_PATCH  0
 
