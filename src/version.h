9e927f7bfcd8dc832edd14b9b77bde6f6db47851
Nat! <nat@mulle-kybernetik.com>
2020-04-19 01:47:42 +0200

    * remove duplicate objc-loader.inc

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define MULLE_FOUNDATION_VERSION   ((0 << 20) | (17 << 8) | 1)
+#define MULLE_FOUNDATION_VERSION   ((0 << 20) | (17 << 8) | 2)
 
 // must be kept like this, this is how the compiler reads it
 // only code that includes the foundation header will get the
