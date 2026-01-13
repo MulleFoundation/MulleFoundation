c3b534543ed5abab41b9b7d41c89d5b420db5646
Nat! <nat@mulle-kybernetik.com>
2026-01-13 22:47:13 +0100

    maintenance: update version to 0.22.3
    Updated clib.json version from 0.22.1 to 0.22.3. Added explicit dependency on MulleFoundationBase. Updated mulle-c tooling versions to 0.19.0 and mulle-objc versions to 0.28.0 in .mulle/share/sde/version files.

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
-#define MULLE_FOUNDATION_VERSION   ((0UL << 20) | (22 << 8) | 3)
+#define MULLE_FOUNDATION_VERSION   ((0UL << 20) | (22 << 8) | 4)
 
 // must be kept like this, this is how the compiler reads it
 // only code that includes the foundation header will get the
 // benefit of version checking at runtime!
 
 #define MULLE_FOUNDATION_VERSION_MAJOR  0
-#define MULLE_FOUNDATION_VERSION_MINOR  17
-#define MULLE_FOUNDATION_VERSION_PATCH  0
+#define MULLE_FOUNDATION_VERSION_MINOR  22
+#define MULLE_FOUNDATION_VERSION_PATCH  4
 
